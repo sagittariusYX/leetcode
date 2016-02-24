8f76cdb620b6816d2412ca38463b93fb01d22522
YXsagittarius <yangxiao@getqujing.com>
2016-02-24 18:07:32 +0800

    Sorted array binary search.

---
+++ ruby/binary_search.rb
@@ -0,0 +1,18 @@
+def search_middle(arr, key)
+  left, right = 0, arr.size - 1
+
+  while left <= right
+    middle = left + (right - left)/2
+    if arr[middle] > key
+      right = middle - 1
+    elsif arr[middle] < key
+      left = middle + 1
+    else
+      return middle
+    end
+  end
+
+  return -1
+end
+
+puts search_middle([10,57,68,70,78,90,122], 78)
