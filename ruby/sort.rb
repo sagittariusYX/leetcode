a3b237411012cdf1400d67139cf73b1fe8b9c874
sagittariusYX <ssdut.xiao@gmail.com>
2016-03-20 15:58:14 +0800

    add sort_algorithms - bubble sort.

--- ruby/sort.rb
+++ ruby/sort.rb
@@ -2,4 +2,16 @@ def quick_sort(array)
   (x=array.pop) ? quick_sort(array.select{|i| i<=x}) + [x] + quick_sort(array.select{|i| i>x}) : []
 end
 
-p quick_sort([72, 6, 57, 88, 60, 42, 83, 73, 48, 65])
+def bubble_sort(array)
+  for i in 0...array.size
+    for j in 0...array.size-i-1
+      if array[j] > array[j+1]
+        array[j], array[j+1] = array[j+1], array[j]
+      end
+    end
+  end
+  return array
+end
+
+#p quick_sort([72, 6, 57, 88, 60, 42, 83, 73, 48, 65])
+p bubble_sort([72, 6, 57, 88, 60, 42, 83, 73, 48, 65])
