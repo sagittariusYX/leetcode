a13741af4444c861bc2fa2350e0faf8c2bb33af9
sagittariusYX <ssdut.xiao@gmail.com>
2016-03-20 17:15:41 +0800

    add sort_algorithms - merge sort.

--- ruby/sort.rb
+++ ruby/sort.rb
@@ -1,8 +1,10 @@
-def quick_sort(array)
+# ===== Quick Sort =====
+def quick_sort array
   (x=array.pop) ? quick_sort(array.select{|i| i<=x}) + [x] + quick_sort(array.select{|i| i>x}) : []
 end
 
-def bubble_sort(array)
+# ===== Bubble Sort =====
+def bubble_sort array
   for i in 0...array.size
     for j in 0...array.size-i-1
       if array[j] > array[j+1]
@@ -13,5 +15,25 @@ def bubble_sort(array)
   return array
 end
 
-#p quick_sort([72, 6, 57, 88, 60, 42, 83, 73, 48, 65])
-p bubble_sort([72, 6, 57, 88, 60, 42, 83, 73, 48, 65])
+# ===== Merge Sort =====
+def merge left, right
+  final = []
+  until left.empty? or right.empty?
+    final << (left.first < right.first ? left.shift : right.shift)
+  end
+  final + left + right
+end
+
+def merge_sort array
+  return array if array.size < 2
+
+  left = array.shift((array.size + 1) >> 1)
+  right = array
+  merge(merge_sort(left), merge_sort(right))
+end
+
+# ===== Check Results
+demo_array = [72, 6, 57, 88, 60, 42, 83, 73, 48, 65]
+# p quick_sort demo_array
+# p bubble_sort demo_array
+p merge_sort demo_array
