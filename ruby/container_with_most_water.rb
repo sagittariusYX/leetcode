f050aff5c5f80818d1b99b9dad5d15ddcaa1bbea
sagittariusYX <ssdut.xiao@gmail.com>
2016-03-27 22:19:02 +0800

    Add explaination of container with most water.

--- ruby/container_with_most_water.rb
+++ ruby/container_with_most_water.rb
@@ -4,6 +4,9 @@ def max_area(height)
   l, r, width, result = 0, height.size - 1, height.size - 1, 0
   (0..width).to_a.reverse_each do |w|
     if height[l] < height[r]
+      # 此时height[l]是短板
+      # 无论height[r]怎样移动 都不可能产生比此时更大的面积
+      # else情况也一样
       result, l = [result, height[l]*w].max, l + 1
     else
       result, r = [result, height[r]*w].max, r - 1
