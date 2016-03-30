d27a14db6fa1a95c071d6662943377bd36c4a7f0
sagittariusYX <ssdut.xiao@gmail.com>
2016-03-30 18:10:59 +0800

    Given a linked list, return the node where the cycle begins.

--- ruby/linked_list_cycle_II.rb
+++ ruby/linked_list_cycle_II.rb
@@ -7,15 +7,20 @@
 # end
 
 # @param {ListNode} head
-# @return {Boolean}
-def has_cycle(head)
+# @return {ListNode}
+def detect_cycle(head)
   slow, fast = head, head
   while fast and fast.next
     fast = fast.next.next
     slow = slow.next
     if slow == fast
-      return true
+      while head
+        if head == slow
+          return head
+        end
+        head, slow = head.next, slow.next
+      end
     end
   end
-  return false
+  return nil
 end
