5b3f1e46b8c25ecb36c20961f420d9450327d208
sagittariusYX <ssdut.xiao@gmail.com>
2016-07-15 13:53:58 +0800

    Follow up for "Search in Rotated Sorted Array"

---
+++ ruby/search_in_rotated_sorted_array_II.rb
@@ -0,0 +1,27 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def search(nums, target)
+  left, right = 0, nums.size - 1
+  while left <= right
+    mid = left + ((right - left) >> 1)
+    if nums[mid] == target
+      return true
+    elsif nums[mid] > nums[left]
+      if nums[left] <= target and target <= nums[mid]
+        right = mid - 1
+      else
+        left = mid + 1
+      end
+    elsif nums[mid] < nums[left]
+      if nums[mid] <= target and target <= nums[right]
+        left = mid + 1
+      else
+        right = mid - 1
+      end
+    else
+      left += 1
+    end
+  end
+  return false
+end
