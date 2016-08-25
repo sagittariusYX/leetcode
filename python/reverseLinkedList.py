6f4398e8f7e9cc151993c845ccc220e98c65c0f5
sagittariusYX <14210240102@fudan.edu.cn>
2016-08-26 05:33:55 +0800

    reverse single linked list

---
+++ python/reverseLinkedList.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pre, right = None, None
+        while head:
+            right = head.next
+            
+            head.next = pre
+            pre = head
+            head = right
+
+        return pre
